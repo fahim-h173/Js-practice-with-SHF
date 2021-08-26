0aff08334785d04bb124b922f869e8a3f4a85db3
Muhammad Fahim <fahimmia616@gmail.com>
2021-08-26 21:31:19 +0600

    All file Added

---
+++ main.js
@@ -0,0 +1,7 @@
+var user= new Array ("Fahim",30,"mym");
+var user2 = Array(54,34,6)
+var user3 = [34,9,33]
+console.log(user,user2,user3);
+for (var x in user2) {
+    document.write(user2[x]+'<br>')
+}
